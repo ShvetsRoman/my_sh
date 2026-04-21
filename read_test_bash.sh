d2e69bbc3bed092a6b3b2b1ed41fd2f54fe036a6
ShvetsRoman <shvets33@gmail.com>
2026-04-21 23:00:48 +0300

    add

--- read_test_bash.sh
+++ read_test_bash.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-read -p "Яке ваше імʼя?" NAME
-read -p "Яке ваше прізвище?" PR
+read -p "Яке ваше імʼя? " NAME
+read -p "Яке ваше прізвище? " PR
+read -s -p "Ввидіть рароль: " PASS
 
+echo " "
 echo "Ваше імʼя ${NAME}"
 echo "Ваше прізвище ${PR}"
+
+echo " "
+echo "Пароль отримано"
 
 
